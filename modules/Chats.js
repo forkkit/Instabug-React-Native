ce473a8f5b4910ffbb70c20a6fee3f2ee5170a91
Ali Abdelfattah <ali.abdelfattah.xa@gmail.com>
2019-08-22 21:35:12 +0200

    💡 Deprecate `IBGChats` module APIs

--- modules/Chats.js
+++ modules/Chats.js
@@ -8,7 +8,9 @@ import {
  * @exports Chats
  */
 export default {
+    /* istanbul ignore next */
     /**
+     * @deprecated See https://docs.instabug.com/docs/ios-sdk-8-6-migration-guide#section-show for instructions on migrating to SDK v8.6 APIs.
      * Enables and disables everything related to creating new chats.
      * @param {boolean} isEnabled 
      */
@@ -16,7 +18,9 @@ export default {
         IBGChats.setEnabled(isEnabled);
     },
 
+    /* istanbul ignore next */
     /**
+     * @deprecated See https://docs.instabug.com/docs/ios-sdk-8-6-migration-guide#section-show for instructions on migrating to SDK v8.6 APIs.
      * Manual invocation for chats view. 
      */
     show() {
